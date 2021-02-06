bfae37b4ee374c79c047ee62bd4ce59d390be857
MarinaUVP <marina.kovac.galicija@gmail.com>
2021-02-06 18:36:57 +0100

    Volumes with a single lysosome

--- statistical_shape_models.py
+++ statistical_shape_models.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
 import nibabel as nib   # https://nipy.org/nibabel/nibabel_images.html#loading-and-saving
-# import matplotlib.pyplot as plt
 # import nrrd
 
 
@@ -161,6 +160,7 @@ def join_candidates_from_slices(array_with_candidates):
 
     return final_group
 
+
 def join_dictionaries(dict1, dict2):
     keys = set()
     keys.update(dict1.keys())
@@ -178,7 +178,6 @@ def join_dictionaries(dict1, dict2):
     return joined_dict
 
 
-
 def join_groups_from_slices(groups_in_slices):
 
     final_groups = []
@@ -192,36 +191,20 @@ def join_groups_from_slices(groups_in_slices):
                 final_groups.append(group)
                 open_groups = []
         else:
-            # if len(open_groups) == 0:  # array with open groups is empty -> currently no open groups
-            #     # add groups from current slice
-            #     for group in slice:
-            #         print("Group")
-            #         print(group)
-            #         open_groups.append({count: group})
-            # else:  # there are some open groups
-            # open_groups_in_current_slice = [] #22
             for current_group in slice:
                 candidates = []
                 # two groups from two consecutive slices will be joined in one if there is at least one element
                 # with the same row index and the same index inside the row (element index)
                 for open_group in open_groups:
-                    # for key in current_group.keys():
-                    #     if key in open_group.keys():  # line/row match
-                    #         indexes_in_line_current = current_group[key]
-                    #         indexes_in_line_candiate = open_group[key]
-                    #         for el in indexes_in_line_current:
-                    #             if el in indexes_in_line_candiate:  # element match
-                    #                 candidates.append(open_group)
-                    #                 break
-                    if count-1 in open_group.keys(): # slice match (group in previous slice)
+                    if count-1 in open_group.keys():  # slice match (group in previous slice)
                         row_indexes_current = current_group.keys()
-                        row_indexes_candiate = open_group[count-1].keys()
-                        for el in row_indexes_current:
-                            if el in row_indexes_candiate:  # row match
-                                element_indexes_current = current_group[el]
-                                element_indexes_candidate = open_group[count-1][el]
+                        row_indexes_candidate = open_group[count-1].keys()
+                        for index in row_indexes_current:
+                            if index in row_indexes_candidate:  # row match
+                                element_indexes_current = current_group[index]
+                                element_indexes_candidate = open_group[count-1][index]
                                 for el in element_indexes_current:
-                                    if el in element_indexes_candidate: # element match
+                                    if el in element_indexes_candidate:  # element match
                                         candidates.append(open_group)
                                         break
                                 break
@@ -229,29 +212,23 @@ def join_groups_from_slices(groups_in_slices):
                 # check number of candidates (number of groups with the same keys)
                 # remove candidates from open_groups to open_groups_in_current_slice
                 if len(candidates) == 0: # no candidates -> new open group
-                    # open_groups_in_current_slice.append({count: current_group})  #22
                     open_groups.append({count: current_group})
                 elif len(candidates) == 1:
                     candidate = candidates[0]
-                    # open_groups.remove(candidate) #22
                     if count in candidate.keys():  # if candidate already contains some group from current slice
-                        join_dictionaries(candidate[count], current_group)
+                        candidate[count] = join_dictionaries(candidate[count], current_group)
                     else:
+                        open_groups.remove(candidate)
                         candidate[count] = current_group
-                    # open_groups_in_current_slice.append(candidate) #22
-                    open_groups.append(candidate)
+                        open_groups.append(candidate)
                 else: # more than 1 candidate
-                    # for candidate in candidates:  #22
-                    #     open_groups.remove(candidate) #22
                     joined_group = join_candidates_from_slices(candidates)
                     joined_group[count] = current_group
-                    # open_groups_in_current_slice.append(joined_group) #22
+                    for candidate in candidates:
+                        open_groups.remove(candidate)
                     open_groups.append(joined_group)
 
             # check if there are groups that need to be closed -> no parts in current slice
-            # for group in open_groups: #22
-            #     final_groups.append(group)  #22
-            # open_groups = open_groups_in_current_slice #22
             for group in open_groups:
                 if count not in group.keys():
                     final_groups.append(group)
@@ -263,46 +240,46 @@ def join_groups_from_slices(groups_in_slices):
 
 # ==================
 directory = os.getcwd()
-path = directory + R"\Podatki\lyso"
-filename = os.path.join(path, 'fib1-0-0-0.nii.gz')
+path = directory + R"\Obtained_raw_data\lyso"
+files = os.listdir(path)
+for file in files:
+    filename = os.path.join(path, file)
 
-# Image data
-img = nib.load(filename)
-img_data = img.get_fdata()
-header = img.header
-data_shape = header.get_data_shape()
-no_slices = data_shape[0]
-no_rows = data_shape[1]
-no_elements = data_shape[2]
+    dot_index = file.find(".")
+    filename_no_ext = file[:dot_index]
 
-# create empty matrix
-empty_matrix = np.zeros((no_slices, no_rows, no_elements))
+    # Image data
+    img = nib.load(filename)
+    img_data = img.get_fdata()
+    header = img.header
+    data_shape = header.get_data_shape()
+    no_slices = data_shape[0]
+    no_rows = data_shape[1]
+    no_elements = data_shape[2]
 
-neighbours_in_slices = find_neighbours_in_slices(img_data)
-final_groups = join_groups_from_slices(neighbours_in_slices)
+    neighbours_in_slices = find_neighbours_in_slices(img_data)
+    final_groups = join_groups_from_slices(neighbours_in_slices)
 
-# experimental = final_groups[0]
-# for el in experimental:
-#     print(str(el) + ":  " + str(experimental[el]))
-#
-with open("final_groups_2.txt", "wt") as f:
-    st = 0
+    count = 1
+
     for group in final_groups:
-        f.write("New Group " + str(st) + "\n")
-        for el in group:
-            f.write(str(el) + "\n")
-            f.write(group[el] + "\n")
-        # f.write(str(group) + "\n")
 
-        # for line_groups in slice_groups:
-        #     f.write(" --- New line ---" + "\n")
-        #     for group in line_groups:
-        #         f.write(str(group)  + "\n")
+        # create empty matrix
+        empty_matrix = np.zeros((no_slices, no_rows, no_elements))
 
+        # change 0s to 1s
+        for slice_ind in group:
+            row = group[slice_ind]
+            for row_index in row:
+                elements = row[row_index]
+                for el_index in elements:
+                    empty_matrix[slice_ind][row_index][el_index] = 1
 
+        name = filename_no_ext + "_" + str(count)
+        ni_img = nib.Nifti1Image(empty_matrix, img.affine)
+        nib.save(ni_img, name)
+        count += 1
 
-# Turn into numpy array
-#array = np.array(img.dataobj)
 
 # Save NRRD
 # nrrd_path_to = "image.nrrd"
