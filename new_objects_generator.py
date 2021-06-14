627ba80c6b3c7a8b33c3c992e7edd857a1911a66
MarinaUVP <marina.kovac.galicija@gmail.com>
2021-06-14 23:02:57 +0200

    Updated algorithm to generate new lysosomes

--- new_objects_generator.py
+++ new_objects_generator.py
@@ -4,19 +4,20 @@ import numpy as np
 import math
 from perlin_noise import PerlinNoise
 import icosahedron
+from random import *
 
 
-def generate_noise(no_ref_points, no_octaves=10):
-    """
-    Generates Perlin noise based on number of reference points.
-    Noise represented as an array of values, same length as no. of reference points.
-    """
-
-    noise = PerlinNoise(octaves=no_octaves, seed=no_ref_points)
-    noise_array = [noise([j/no_ref_points]) for j in range(no_ref_points)]
-    # edited_array = [el*10 for el in noise_array]
-
-    return noise_array
+# def generate_noise(no_ref_points, no_octaves=10):
+#     """
+#     Generates Perlin noise based on number of reference points.
+#     Noise represented as an array of values, same length as no. of reference points.
+#     """
+#
+#     noise = PerlinNoise(octaves=no_octaves, seed=no_ref_points)
+#     noise_array = [noise([j/no_ref_points]) for j in range(no_ref_points)]
+#     # edited_array = [el*10 for el in noise_array]
+#
+#     return noise_array
 
 
 def read_intersections_file(path_to_file):
@@ -53,35 +54,38 @@ def unite_by_ref_index(intersections):
     return ref_dict
 
 
-# def compute_avg_vector(vec_array):
+# def avg_vect_by_ref(ref_dict):
 #     """
-#     Compute average vector from array of vectors (vec_array).
+#     Computes average vector for each ref. direction.
+#     Returns a list of average vectors.
 #     """
-#     avg_vec = np.array([0, 0, 0])
 #
-#     for arr in vec_array:
-#         avg_vec = avg_vec + np.array(arr)
+#     avg_ref_vectors = []
+#     no_ref_points = len(ref_dict.keys())
 #
-#     avg_vec = avg_vec / len(vec_array)
+#     for i in range(no_ref_points):
+#         points = ref_dict[i]
+#         avg = np.average(points, axis=0)
+#         avg_ref_vectors.append(avg)
 #
-#     return avg_vec
+#     return avg_ref_vectors
 
 
-def avg_vect_by_ref(ref_dict):
+def dict_avg_vect_by_ref(ref_dict):
     """
     Computes average vector for each ref. direction.
-    Returns a list of average vectors.
+    Returns a dictionary of avg vectors.
+    {ref_index: avg_vector}
     """
 
-    avg_ref_vectors = []
-    no_ref_points = len(ref_dict.keys())
+    avg_ref_vectors_dict = dict()
 
-    for i in range(no_ref_points):
-        points = ref_dict[i]
+    for key in (ref_dict.keys()):
+        points = ref_dict[key]
         avg = np.average(points, axis=0)
-        avg_ref_vectors.append(avg)
+        avg_ref_vectors_dict[key] = avg
 
-    return avg_ref_vectors
+    return avg_ref_vectors_dict
 
 
 def min_max_one_ref_point(points_array):
@@ -127,27 +131,119 @@ def min_max_all_ref_points(ref_dict):
     return min_max_array
 
 
-def min_max_directions(avg_vectors, mins_maxs_list):
+def standard_deviation_all_ref_points(ref_dict):
     """
-    Computes vectors from average vector to min and max vectors for each reference point.
+    Computes standard deviation for each reference point.
     """
 
-    no_ref_points = len(avg_vectors)
-    directions = dict()
+    avg_vectors = dict_avg_vect_by_ref(ref_dict)
+
+    std_dev = dict()
+
+    for key in list(ref_dict.keys()):
+        points = ref_dict[key]
+        avg_vect = avg_vectors[key]
+        squared_devs = 0
+
+        for point in points:
+            squared_devs += np.square(avg_vect - np.array(point))
+
+        std_dev[key] = np.sqrt(squared_devs / len(points))
+
+    return std_dev
+
+
+# def borders_ref_point(avg_vectors, std_dev):
+#     """
+#     Compoutes borders of area where values can be selected. For each reference point.
+#     [avg. vector - std. deviation, avg. vector + std. deviation]
+#     :param avg_vectors:  Dictionary of avg. vectors (based on indexes).
+#     :param std_dev: Dictionary of standard deviation (based on indexes)
+#     :return: Dictionary of borders for each ref. point.
+#     """
+#
+#     borders_dict = dict()
+#
+#     for key in list(avg_vectors.keys()):
+#         borders_dict[key] = [avg_vectors[key] - st_dev[key], avg_vectors[key] - st_dev[key]]
+#
+#     return borders_dict
+
+def min_max_arrays_all_ref_points(ref_dict):
+    """
+    Finds min and max vector/point for all reference points.
+    """
+    no_ref_points = len(ref_dict.keys())
+
+    mins = []
+    maxs = []
 
     for ind in range(no_ref_points):
-        avg_vector = avg_vectors[ind]
-        min_vector, max_vector = mins_maxs_list[ind]
+        points = ref_dict[ind]
+        min, max = min_max_one_ref_point(points)
+        mins.append(min)
+        maxs.append(max)
 
-        min_direction = min_vector - avg_vector
-        max_direction = max_vector - avg_vector
-
-        directions[ind] = [min_direction, max_direction]
-
-    return directions
+    return mins, maxs
 
 
-def generate_lyso(data_file, param=0):
+# def min_max_directions(avg_vectors, mins_maxs_list):
+#     """
+#     Computes vectors from average vector to min and max vectors for each reference point.
+#     """
+#
+#     no_ref_points = len(avg_vectors)
+#     directions = dict()
+#
+#     for ind in range(no_ref_points):
+#         avg_vector = avg_vectors[ind]
+#         min_vector, max_vector = mins_maxs_list[ind]
+#
+#         min_direction = min_vector - avg_vector
+#         max_direction = max_vector - avg_vector
+#
+#         directions[ind] = [min_direction, max_direction]
+#
+#     return directions
+
+
+# def generate_lyso(data_file, param=0):
+#     """
+#     Generates new lysosome.
+#     :param param: Value between -1 and 1.
+#     :return: Data for mesh object. List of vertices and list of faces.
+#     """
+#
+#     # Icosahedron -----
+#     ico_vertices = icosahedron.icosahedron_vertices()
+#     ico_faces = icosahedron.icosahedron_faces()
+#     sub_ico_vertices, sub_ico_faces = icosahedron.subdivided_icosahedron(ico_vertices, ico_faces, 3)
+#
+#     # Obtained data -----
+#     all_intersection = read_intersections_file(data_file)
+#     u_dict = unite_by_ref_index(all_intersection)
+#     avg_vectors = avg_vect_by_ref(u_dict)
+#     mins_maxs_list = min_max_all_ref_points(u_dict)
+#     min_max_directs = min_max_directions(avg_vectors, mins_maxs_list)
+#
+#     new_vertices = []
+#     if param == 0:
+#         new_vertices = avg_vectors
+#     else:
+#         if param > 0:
+#             for ind in range(len(avg_vectors)):
+#                 point = avg_vectors[ind] + param * min_max_directs[ind][1]
+#                 new_vertices.append(point)
+#         elif param < 0:
+#             for ind in range(len(avg_vectors)):
+#                 point = avg_vectors[ind] + abs(param) * min_max_directs[ind][0]
+#                 new_vertices.append(point)
+#
+#     return new_vertices, sub_ico_faces
+#
+
+
+def lyso_generator_old(data_file, param=0, delta = 0.5):
     """
     Generates new lysosome.
     :param param: Value between -1 and 1.
@@ -162,30 +258,54 @@ def generate_lyso(data_file, param=0):
     # Obtained data -----
     all_intersection = read_intersections_file(data_file)
     u_dict = unite_by_ref_index(all_intersection)
-    avg_vectors = avg_vect_by_ref(u_dict)
-    mins_maxs_list = min_max_all_ref_points(u_dict)
-    min_max_directs = min_max_directions(avg_vectors, mins_maxs_list)
+    # avg_vectors = avg_vect_by_ref(u_dict)
+    # mins_maxs_list = min_max_all_ref_points(u_dict)
+    # min_max_directs = min_max_directions(avg_vectors, mins_maxs_list)
+    mins, maxs = min_max_arrays_all_ref_points(u_dict)
+    no_vertices = len(mins)
+
+    # Value between 0 and 1 based on the param
+    seed(param)
+    value = random()
+
+    # check delta and adjust it if necessary
+    closest_edge = min(value, 1-value)
+    if closest_edge < delta:
+        delta = closest_edge
+
+    # generate noise array with value as mean and delta as standard deviation
+    np.random.seed(param)
+    noised_values = np.random.normal(value, delta, no_vertices)
 
     new_vertices = []
-    if param == 0:
-        new_vertices = avg_vectors
-    else:
-        if param > 0:
-            for ind in range(len(avg_vectors)):
-                point = avg_vectors[ind] + param * min_max_directs[ind][1]
-                new_vertices.append(point)
-        elif param < 0:
-            for ind in range(len(avg_vectors)):
-                point = avg_vectors[ind] + abs(param) * min_max_directs[ind][0]
-                new_vertices.append(point)
+    for ind in range(no_vertices):
+        direction = maxs[ind] - mins[ind]
+        point = mins[ind] + noised_values[ind] * direction
+        new_vertices.append(point)
 
     return new_vertices, sub_ico_faces
 
 
-def generate_lyso_with_noise(data_file, param=0):
+def normalize_values(values):
+    """
+    Make sure all values are in range between 0 and 1.
+    """
+    new_valeus = []
+    for el in values:
+        if el < 0:
+            el = 0
+        if el > 1:
+            el = 1
+        new_valeus.append(el)
+    return new_valeus
+
+
+def lyso_generator(data_file, param=0, sigma=0.2):
     """
     Generates new lysosome.
-    :param param: Value between -1 and 1.
+    :param data_file:
+    :param param: Whole number.
+    :param sigma: Standard deviation of the probability density function of the normal distribution.
     :return: Data for mesh object. List of vertices and list of faces.
     """
 
@@ -197,29 +317,25 @@ def generate_lyso_with_noise(data_file, param=0):
     # Obtained data -----
     all_intersection = read_intersections_file(data_file)
     u_dict = unite_by_ref_index(all_intersection)
-    avg_vectors = avg_vect_by_ref(u_dict)
-    mins_maxs_list = min_max_all_ref_points(u_dict)
-    min_max_directs = min_max_directions(avg_vectors, mins_maxs_list)
+    avg_vectors = dict_avg_vect_by_ref(u_dict)
+    st_devs = standard_deviation_all_ref_points(u_dict)
+    no_vertices = len(list(avg_vectors.keys()))
 
-    # Noise -----
-    no_ref_points = len(avg_vectors)
-    noise = generate_noise(no_ref_points)
+    # Generate values between 0 and 1 based on the param.
+    np.random.seed(param)
+    values = np.random.normal(0.5, sigma, no_vertices)
+    n_values = normalize_values(values)
+    print("n values")
+    print(n_values)
 
+    # Generate vertices of new object
     new_vertices = []
-    if param == 0:
-        new_vertices = avg_vectors
-    else:
-        if param > 0:
-            for ind in range(len(avg_vectors)):
-                point = avg_vectors[ind] + param * min_max_directs[ind][1] + noise[ind]
-                new_vertices.append(point)
-        elif param < 0:
-            for ind in range(len(avg_vectors)):
-                point = avg_vectors[ind] + abs(param) * min_max_directs[ind][0] + noise[ind]
-                new_vertices.append(point)
+    for key in list(avg_vectors.keys()):
+        whole_vector = 2 * np.array(st_devs[key])
+        new_point = (avg_vectors[key] - st_devs[key]) + n_values[key] * whole_vector
+        new_vertices.append(new_point)
 
     return new_vertices, sub_ico_faces
-
 
 def write_obj_file(filepath, vertices, faces):
     """
@@ -246,21 +362,24 @@ def write_obj_file(filepath, vertices, faces):
 directory = os.getcwd()
 path_data_file = directory + R"\Lyso_single\Intersections\all_intersections_cog_iso_lyso_3.txt"
 path_new_objects = directory + R"\Lyso_single\New_objects\sub_3"
-filename = "new_lyso_0.obj"
+filename = "new_lyso_example_987_0.03.obj"
 
 path_new_obj = os.path.join(path_new_objects, filename)
 
-vertices, faces = generate_lyso(path_data_file, 0)
+vertices, faces = lyso_generator(path_data_file, 987, 0.03)
+# vertices, faces = lyso_generator_old(path_data_file, 987, 0.01)
 write_obj_file(path_new_obj, vertices, faces)
 
 
-# === Noise
+# === Experiments
 
-# v = 642
-# n10 = generate_noise(42, 10)
-# n = [el*10 for el in n10]
-# print(n10)
-# print(n)
+# all_intersection = read_intersections_file(path_data_file)
+# u_dict = unite_by_ref_index(all_intersection)
+# avg_dict = dict_avg_vect_by_ref(u_dict)
+# st_dev = standard_deviation_all_ref_points(u_dict)
+#
+# for key in list(avg_dict.keys()):
+#     print(key, avg_dict[key])
 
 
 
